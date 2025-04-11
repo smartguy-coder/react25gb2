6cc143f84529176a0bc6b3aa3251d6870d91cd5a
Vasyl <vasyl@example.com>
2025-04-12 00:45:49 +0300

    L-35-39

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function TextArea() {
+  const [text, setText] = useState("");
+  const [warningText, setWarningText] = useState("");
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "");
+      setWarningText("Script tag detected!");
+    } else if (newText.includes("@")) {
+      newText = newText.replace("@", "");
+      setWarningText("Email detected!");
+    } else {
+      setWarningText("");
+    }
+    setText(newText);
+  };
+
   return (
-    <textarea
-      onChange={(e) => console.log(e.target.value)}
-      className="textarea"
-      placeholder="Type your text here..."
-      spellCheck="false"
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Type your text here..."
+        spellCheck="true"
+      />
+      <Warning warningText={warningText} />
+    </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["a0a9-188-130-177-189.ngrok-free.app"], // Дозволяє всі ngrok-free домени
+  },
+});
