66b0a13f691f2da0efb5843afb9116218d3d75b0
Vasyl <vasyl@example.com>
2025-04-13 01:23:52 +0300

    L-40-45 stats

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,23 @@
-export default function Stats() {
-  return <section className="stats">Stats</section>;
+function Stat({ label, number }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <h2 className="second-heading">{label}</h2>
+    </section>
+  );
+}
+
+export default function Stats({ text }) {
+  const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
+  const numberOfCharacters = text.length;
+  const instagramCharacrsLeft = 280 - numberOfCharacters;
+  const facebookCharacrsLeft = 2200 - numberOfCharacters;
+  return (
+    <section className="stats">
+      <Stat label={"Words"} number={numberOfWords} />
+      <Stat label={"Characters"} number={numberOfCharacters} />
+      <Stat label={"Instagram"} number={instagramCharacrsLeft} />
+      <Stat label={"Facebook"} number={facebookCharacrsLeft} />
+    </section>
+  );
 }
