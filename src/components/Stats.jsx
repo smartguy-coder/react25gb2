ae53febd8045d0a277dd18072d2165e41763a78a
Vasyl <vasyl@example.com>
2025-04-13 13:03:19 +0300

    L46-48 finish

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,16 @@
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from "../lib/constants";
+
 function Stat({ label, number }) {
   return (
     <section className="stat">
-      <span className="stat__number">{number}</span>
+      <span
+        className={`stat__number ${number < 0 ? "stat__number--limit" : ""}`}
+      >
+        {number}
+      </span>
       <h2 className="second-heading">{label}</h2>
     </section>
   );
@@ -10,8 +19,8 @@ function Stat({ label, number }) {
 export default function Stats({ text }) {
   const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
   const numberOfCharacters = text.length;
-  const instagramCharacrsLeft = 280 - numberOfCharacters;
-  const facebookCharacrsLeft = 2200 - numberOfCharacters;
+  const instagramCharacrsLeft = INSTAGRAM_MAX_CHARACTERS - numberOfCharacters;
+  const facebookCharacrsLeft = FACEBOOK_MAX_CHARACTERS - numberOfCharacters;
   return (
     <section className="stats">
       <Stat label={"Words"} number={numberOfWords} />
